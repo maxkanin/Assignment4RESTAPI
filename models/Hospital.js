7885ceb3cc10ede0d597192a2f3af4bb270179b4
kanin <kanin49144@gmail.com>
2023-02-04 15:33:25 +0700

    Connect with mongoDB Atlas

---
+++ models/Hospital.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const HospitalSchema= new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true,'Please Add a name'],
+        unique: true,
+        trim:true,
+        maxlength:[50,'Name can not be more than 50 characters']
+    },
+    address:{
+        type: String,
+        required:[true,'Please Add an address'],
+    },
+    district:{
+        type: String,
+        required:[true,'Please Add a district'],
+    },
+    postalcode:{
+        type: String,
+        required:[true,'Please Add a postalcode'],
+        maxlength:[5,'Postalcode can not be more than 5 digits']
+    },
+    tel:{
+        type: String,
+    },
+    region:{
+        type: String,
+        required:[true,'Please Add a region'],
+    },
+});
+module.exports=mongoose.model('Hospital',HospitalSchema);
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
 
-const hospitals  = require(`./routes/hospitals`);
 
 
 dotenv.config({path:'./config/config.env'});
+//connect to DB
+connectDB();
+//Route file
+const hospitals  = require(`./routes/hospitals`);
 
 const app = express();
 
@@ -18,9 +22,14 @@ const app = express();
 //     res.status(200).json({success:true,data:{id:1}});
 // });
 
-
+app.use(express.json())
 app.use(`/api/v1/hospitals`,hospitals);
 
 
 const PORT = process.env.PORT;
-app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV," mode on port ",PORT));
+const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV," mode on port ",PORT));
+
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`)
+    server.close(()=>process.exit(1))
+})
